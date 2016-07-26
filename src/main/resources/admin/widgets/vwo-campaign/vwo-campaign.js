92cc2d443ce90c6be678bbaaa47dcdbcc42fe585
sshklianko <stefan.shkl@gmail.com>
2016-07-26 17:02:32 +0300

    XP-3762 Implement authentication via VWO - Fixed problem with getting nearest site config

--- src/main/resources/admin/widgets/vwo-campaign/vwo-campaign.js
+++ src/main/resources/admin/widgets/vwo-campaign/vwo-campaign.js
@@ -1,11 +1,18 @@
 var portal = require('/lib/xp/portal');
 var mustacheLib = require('/lib/xp/mustache');
+var contentLib = require('/lib/xp/content');
 
 function handleGet(req) {
     var uid = req.url.split('?uid=')[1];
     var view = resolve('vwo-campaign.html');
-    var siteConfig = portal.getSiteConfig();
-    var completeSetup = siteConfig && !!siteConfig.accountId && !!siteConfig.domain;
+    var contentId = req.params.contentId;
+
+    var siteConfig = contentLib.getSiteConfig({ // get nearest site config
+        key: contentId,
+        applicationKey: app.name
+    });
+
+    var completeSetup = !!siteConfig && !!siteConfig.accountId && !!siteConfig.domain;
 
     var params = {
         vwoCssUrl: portal.assetUrl({path: 'css/app-vwo.css'}),
